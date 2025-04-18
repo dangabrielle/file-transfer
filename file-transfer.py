6e22dcd2cfb0e6162ed9c2f7669f995e186a0750
dangabrielle <hoangabrielle@gmail.com>
2025-04-17 15:33:56 -1000

    download file, save to desktop to test

---
+++ file-transfer.py
@@ -0,0 +1,45 @@
+import io
+import os.path
+from datetime import datetime
+import os
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+
+def download_file(file_id, file_name):
+    creds = None
+    # Load existing token or go through OAuth flow
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save token for later
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('drive', 'v3', credentials=creds)
+        request = service.files().get_media(fileId=file_id)
+        fh = io.FileIO(file_name, 'wb')
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while not done:
+            status, done = downloader.next_chunk()
+            print(f"Download {int(status.progress() * 100)}%.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+  #test on desktop first
+  download_file("insert google drive file id", f"/Users/YourUser/Desktop/downloaded_file_{datetime.now()}.xlsx")
+
